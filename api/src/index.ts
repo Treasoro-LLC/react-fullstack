02ac39ec5b958ce1ae1b2f8bf50da657feaa2214
Bill Ward <bill.ward@treasoro.com>
2025-03-07 12:23:00 -0600

    prettier works

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,6 @@
 // src/index.ts
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
 import { add } from '@react-fullstack/utils';
 
 dotenv.config();
@@ -8,12 +8,12 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server');
 });
 
 app.listen(port, () => {
-  const result = add(5,10);
-  console.log(`Calling [util/add] 5+10: ${result}`)
+  const result = add(5, 10);
+  console.log(`Calling [util/add] 5+10: ${result}`);
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
